1b864499eb571715ed00650d4daee47b8b379ddf
PepMS <jmarti@iri.upc.edu>
2020-05-30 21:12:39 +0200

    [feature] add test machinery

---
+++ unittest/mission_test.cpp
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MODULE waypoint_test test
+
+#include <boost/test/unit_test.hpp>
+
+// #include "multicopter_mpc/waypoint.hpp"
+
+BOOST_AUTO_TEST_CASE( constructors_test ) {
+  BOOST_CHECK_EQUAL(2, 2);
+}
