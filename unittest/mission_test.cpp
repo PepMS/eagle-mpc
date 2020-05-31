792cdc412431cc5dc75a695cc22956d897c1b2bb
PepMS <jmarti@iri.upc.edu>
2020-05-31 08:16:05 +0200

    [test] mission funcionalities tests done

--- unittest/mission_test.cpp
+++ unittest/mission_test.cpp
@@ -86,8 +86,31 @@ BOOST_AUTO_TEST_CASE(fill_waypoints_waypoint_pose) {
   pos << 0.0, 1.0, 2.5;
   Eigen::Quaterniond quaternion = Eigen::Quaterniond(0, 1, 0, 0);
   Eigen::Vector3d zero = Eigen::Vector3d::Zero();
-  BOOST_CHECK(
-      checkWaypoint(mission.waypoints_[1], knots, pos, quaternion, zero, zero));
+  BOOST_CHECK(checkWaypoint(mission.waypoints_[1], knots, pos, quaternion, zero, zero));
+}
+
+BOOST_AUTO_TEST_CASE(fill_initial_state) {
+  std::size_t nx = 13;
+  multicopter_mpc::Mission mission(nx);
+
+  std::string mission_yaml_path = std::string(_MULTICOPTER_MPC_ROOT_DIR) + "/unittest/config/mission.yaml";
+
+  yaml_parser::ParserYAML yaml_mission(mission_yaml_path, "", true);
+  yaml_parser::ParamsServer server_mission(yaml_mission.getParams());
+  mission.fillInitialState(server_mission);
+
+  Eigen::Vector3d pos;
+  pos << 0.0, 2.0, 2.5;
+  Eigen::Quaterniond quaternion = Eigen::Quaterniond(1, 0, 0, 0);
+  Eigen::Vector3d vel;
+  vel << -3.0, 0.0, 0.0;
+  Eigen::Vector3d rate;
+  rate << 0.0, 0.0, 0.0;
+  BOOST_CHECK(pos == mission.x0_.head(3));
+  BOOST_CHECK(quaternion.toRotationMatrix() ==
+              Eigen::Quaterniond(mission.x0_(6), mission.x0_(3), mission.x0_(4), mission.x0_(5)).toRotationMatrix());
+  BOOST_CHECK(vel == mission.x0_.segment(7, 3));
+  BOOST_CHECK(rate == mission.x0_.segment(10, 3));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
