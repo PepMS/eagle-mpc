d6bd15064042d61054fe32b067331e1a785d09ef
PepMS <jmarti@iri.upc.edu>
2020-06-11 08:43:30 +0200

    [feature] added params yaml parser for the trajectory controller

--- include/multicopter_mpc/ocp/trajectory-generator.hpp
+++ include/multicopter_mpc/ocp/trajectory-generator.hpp
@@ -6,11 +6,30 @@
 
 #include <boost/shared_ptr.hpp>
 
+#include "yaml_parser/params_server.hpp"
+#include "yaml_parser/parser_yaml.h"
+
 #include "multicopter_mpc/ocp-base.hpp"
 #include "multicopter_mpc/mission.hpp"
 #include "multicopter_mpc/multicopter-base-params.hpp"
 
 namespace multicopter_mpc {
+
+struct TrajectoryGeneratorParams {
+  Eigen::Vector3d w_state_position;      // Importance of the position in the state regularization
+  Eigen::Vector3d w_state_orientation;   // Importance of the orientationin the state regularization
+  Eigen::Vector3d w_state_velocity_lin;  // Importance of the linear velocity in the state regularization
+  Eigen::Vector3d w_state_velocity_ang;  // Importance of the angular velocity in the state regularization
+
+  double w_state_running; // General penalization for the state in running models
+  double w_control_running; // General penalization for the control in running models
+  double w_pos_running; // General penalization for the pose error in running models
+  double w_vel_running; // General penalization for the pose error in running models
+  
+  double w_pos_terminal; // General penalization for the pose error in terminal model
+  double w_vel_terminal; // General penalization for the pose error in terminal model
+
+};
 
 class TrajectoryGenerator : public OcpAbstract {
  public:
@@ -19,7 +38,9 @@ class TrajectoryGenerator : public OcpAbstract {
                       const boost::shared_ptr<Mission>& mission);
   ~TrajectoryGenerator();
 
-  void createProblem(const SolverTypes::Type& solver_type) override;
+  void loadParameters(const yaml_parser::ParamsServer& server) override;
+
+  void createProblem(const SolverTypes::Type& solver_type);
 
   boost::shared_ptr<crocoddyl::CostModelAbstract> createCostStateRegularization();
   boost::shared_ptr<crocoddyl::CostModelAbstract> createCostControlRegularization();
@@ -31,9 +52,12 @@ class TrajectoryGenerator : public OcpAbstract {
   const Eigen::VectorXd& getTrajectoryState(const std::size_t& cursor) const;
 
  private:
+  void initializeDefaultParameters() override;
+
   boost::shared_ptr<Mission> mission_;
   std::vector<Eigen::VectorXd> state_trajectory_;
   Eigen::VectorXd state_hover_;
+  TrajectoryGeneratorParams params_;
 };
 }  // namespace multicopter_mpc
 
--- src/ocp/trajectory-generator.cpp
+++ src/ocp/trajectory-generator.cpp
@@ -5,14 +5,50 @@ namespace multicopter_mpc {
 TrajectoryGenerator::TrajectoryGenerator(const boost::shared_ptr<pinocchio::Model> model,
                                          const boost::shared_ptr<MultiCopterBaseParams>& mc_params, const double& dt,
                                          const boost::shared_ptr<Mission>& mission)
-    : OcpAbstract(model, mc_params, dt), mission_(mission) {}
+    : OcpAbstract(model, mc_params, dt), mission_(mission) {
+  n_knots_ = mission_->getTotalKnots();
+
+  initializeDefaultParameters() :
+}
 
 TrajectoryGenerator::~TrajectoryGenerator() {}
 
+void TrajectoryGenerator::initializeDefaultParameters() {
+  params_.w_state_position.fill(1.);
+  params_.w_state_orientation.fill(1.);
+  params_.w_state_velocity_lin.fill(1.);
+  params_.w_state_velocity_ang.fill(1000.);
+
+  params_.w_state_running = 1e-6;
+  params_.w_control_running = 1e-4;
+  params_.w_pos_running = 1e-2;
+  params_.w_vel_running = 1e-2;
+
+  params_.w_pos_terminal = 100;
+  params_.w_vel_terminal = 10;
+}
+
+
+void TrajectoryGenerator::loadParameters(const yaml_parser::ParamsServer& server) {
+  std::vector<std::string> state_weights = server.getParam<std::vector<std::string>>("ocp/state_weights");
+  std::map<std::string, std::string> current_state =
+      yaml_parser::converter<std::map<std::string, std::string>>::convert(state_weights[0]);
+
+  params_.w_state_position = yaml_parser::converter<Eigen::Vector3d>::convert(current_state["position"]);
+  params_.w_state_orientation = yaml_parser::converter<Eigen::VectorXd>::convert(current_state["orientation"]);
+  params_.w_state_velocity_lin = yaml_parser::converter<Eigen::VectorXd>::convert(current_state["velocity_lin"]);
+  params_.w_state_velocity_ang = yaml_parser::converter<Eigen::VectorXd>::convert(current_state["velocity_ang"]);
+
+  params_.w_state_running = server.getParam<double>("ocp/cost_running_state_weight");
+  params_.w_control_running = server.getParam<double>("ocp/cost_running_control_weight");
+  params_.w_pos_running = server.getParam<double>("ocp/cost_running_pos_weight");
+  params_.w_vel_running = server.getParam<double>("ocp/cost_running_vel_weight");
+  params_.w_pos_terminal = server.getParam<double>("ocp/cost_terminal_pos_weight");
+  params_.w_vel_terminal = server.getParam<double>("ocp/cost_terminal_vel_weight");
+}
+
 void TrajectoryGenerator::createProblem(const SolverTypes::Type& solver_type) {
   assert(mission_->waypoints_.size() > 0);
-
-  n_knots_ = mission_->getTotalKnots();
 
   boost::shared_ptr<crocoddyl::CostModelAbstract> cost_reg_state = createCostStateRegularization();
   boost::shared_ptr<crocoddyl::CostModelAbstract> cost_reg_control = createCostControlRegularization();
@@ -25,26 +61,26 @@ void TrajectoryGenerator::createProblem(const SolverTypes::Type& solver_type) {
         boost::make_shared<crocoddyl::CostModelSum>(state_, actuation_->get_nu());
 
     // Regularitzations
-    cost_model_running->addCost("x_reg", cost_reg_state, 1e-6);
-    cost_model_running->addCost("u_reg", cost_reg_control, 1e-4);  // 1e-4
+    cost_model_running->addCost("x_reg", cost_reg_state, params_.w_state_running);      // 1e-6
+    cost_model_running->addCost("u_reg", cost_reg_control, params_.w_control_running);  // 1e-4
     if (std::next(wp) != mission_->waypoints_.end()) {
-      cost_model_terminal->addCost("x_reg_cost", cost_reg_state, 1e-6);
-      cost_model_terminal->addCost("u_reg_cost", cost_reg_control, 1e-4);  // 1e-4
+      cost_model_terminal->addCost("x_reg_cost", cost_reg_state, params_.w_state_running);      // 1e-6
+      cost_model_terminal->addCost("u_reg_cost", cost_reg_control, params_.w_control_running);  // 1e-4
     }
 
     // Waypoint cost related
     crocoddyl::FramePlacement frame_ref = crocoddyl::FramePlacement(frame_base_link_id_, wp->pose);
     boost::shared_ptr<crocoddyl::CostModelAbstract> cost_goal =
         boost::make_shared<crocoddyl::CostModelFramePlacement>(state_, frame_ref, actuation_->get_nu());
-    cost_model_running->addCost("track_pos_cost", cost_goal, 1e-2);
-    cost_model_terminal->addCost("goal_pos_cost", cost_goal, 100);
+    cost_model_running->addCost("track_pos_cost", cost_goal, params_.w_pos_running);   // 1e-2
+    cost_model_terminal->addCost("goal_pos_cost", cost_goal, params_.w_pos_terminal);  // 100
 
     if (wp->vel != boost::none) {
       crocoddyl::FrameMotion vel_ref(frame_base_link_id_, *(wp->vel));
       boost::shared_ptr<crocoddyl::CostModelAbstract> cost_goal_vel =
           boost::make_shared<crocoddyl::CostModelFrameVelocity>(state_, vel_ref, actuation_->get_nu());
-      cost_model_running->addCost("track_vel_cost", cost_goal_vel, 1e-2);
-      cost_model_terminal->addCost("goal_vel_cost", cost_goal_vel, 10);
+      cost_model_running->addCost("track_vel_cost", cost_goal_vel, params_.w_vel_running);   // 1e-2
+      cost_model_terminal->addCost("goal_vel_cost", cost_goal_vel, params_.w_vel_terminal);  // 10
     }
 
     // Diff & Integrated models
@@ -96,10 +132,10 @@ void TrajectoryGenerator::createProblem(const SolverTypes::Type& solver_type) {
 boost::shared_ptr<crocoddyl::CostModelAbstract> TrajectoryGenerator::createCostStateRegularization() {
   Eigen::VectorXd state_weights(state_->get_ndx());
 
-  state_weights.head(3).fill(1.);                        // Position 1.
-  state_weights.segment(3, 3).fill(1.);                  // Orientation 1.
-  state_weights.segment(model_->nv, 3).fill(1.);         // Linear velocity 1.
-  state_weights.segment(model_->nv + 3, 3).fill(1000.);  // Angular velocity 1000.
+  state_weights.head(3) = params_.w_state_position;                         // Position 1.
+  state_weights.segment(3, 3) = params_.w_state_orientation;                // Orientation 1.
+  state_weights.segment(model_->nv, 3) = params_.w_state_velocity_lin;      // Linear velocity 1.
+  state_weights.segment(model_->nv + 3, 3) = params_.w_state_velocity_ang;  // Angular velocity 1000.
 
   boost::shared_ptr<crocoddyl::ActivationModelWeightedQuad> activation_state =
       boost::make_shared<crocoddyl::ActivationModelWeightedQuad>(state_weights);
