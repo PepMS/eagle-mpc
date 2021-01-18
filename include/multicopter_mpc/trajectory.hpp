4e9f381fd973b5b742d52568d77603bef0ea1f4b
PepMS <jmarti@iri.upc.edu>
2021-01-18 11:53:26 +0100

    Missing file from last commit

--- include/multicopter_mpc/trajectory.hpp
+++ include/multicopter_mpc/trajectory.hpp
@@ -5,8 +5,18 @@
 #include <map>
 #include <string>
 
-#include "multicopter_mpc/utils/params_server.hpp"
+#include "pinocchio/parsers/urdf.hpp"
+#include "pinocchio/multibody/model.hpp"
+
+#include "crocoddyl/core/actuation/squashing/smooth-sat.hpp"
+#include "crocoddyl/core/actuation/actuation-squashing.hpp"
+
+#include "crocoddyl/multibody/actuations/multicopter-base.hpp"
+#include "crocoddyl/multibody/states/multibody.hpp"
+
+#include "multicopter_mpc/multicopter-base-params.hpp"
 #include "multicopter_mpc/stage.hpp"
+#include "multicopter_mpc/utils/params_server.hpp"
 
 namespace multicopter_mpc {
 class Trajectory {
@@ -16,8 +26,19 @@ class Trajectory {
 
   void autoSetup(const ParamsServer& server);
 
+  const boost::shared_ptr<pinocchio::Model>& get_robot_model();
+
  private:
- std::map<std::string, Stage> stages_;
+  std::map<std::string, Stage> stages_;
+
+  boost::shared_ptr<pinocchio::Model> robot_model_;
+
+  boost::shared_ptr<MultiCopterBaseParams> platform_params_;
+
+  boost::shared_ptr<crocoddyl::StateMultibody> robot_state_;
+  boost::shared_ptr<crocoddyl::ActuationModelMultiCopterBase> actuation_;
+  boost::shared_ptr<crocoddyl::SquashingModelSmoothSat> squash_;
+  boost::shared_ptr<crocoddyl::ActuationSquashingModel> actuation_squash_;
 };
 }  // namespace multicopter_mpc
 
