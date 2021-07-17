2ec7de60e5c8ab3362df23e6243f28173eb11e9b
PepMS <jmarti@iri.upc.edu>
2021-07-17 18:49:31 +0200

    [cmake] added library to the -config.cmake

--- cmake/eagle_mpc-config.cmake
+++ cmake/eagle_mpc-config.cmake
@@ -1,3 +1,6 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/eagle_mpc.cmake)
 set(eagle_mpc_INCLUDE_DIRS "/usr/local/include")
+set(EAGLE_MPC_INCLUDE_DIRS "/usr/local/include")
+set(eagle_mpc_LIBRARIES "/usr/local/lib/libeagle_mpc.so")
+set(EAGLE_MPC_LIBRARIES "/usr/local/lib/libeagle_mpc.so")
